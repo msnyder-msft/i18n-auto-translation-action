a1fbd72a91ab0f73a25e91e43c96c9bbe39a6d62
Mitch Snyder <msnyder@microsoft.com>
2022-04-11 12:43:13 -0700

    Updates

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const childProcess = require('child_process');
 
 try {
   const provider = core.getInput('provider');
@@ -9,20 +10,42 @@ try {
   const dirPath = core.getInput('dirPath');
   const from = core.getInput('from');
   const to = core.getInput('to');
-  console.log(`provider ${provider}!`);
-  console.log(`subscriptionKey ${subscriptionKey}!`);
-  console.log(`location ${location}!`);
-  console.log(`filePath ${filePath}!`);
-  console.log(`dirPath ${dirPath}!`);
-  console.log(`from ${from}!`);
-  console.log(`to ${to}!`);
+  const debug = core.getInput('debug') === true;
+  console.log(`***Starting translation using the ${provider} provider`);
 
-  // console.log(`Hello ${provider}!`);
-  // const time = (new Date()).toTimeString();
+  if (debug) {
+    console.log(`provider ${provider}`);
+    console.log(`subscriptionKey ${subscriptionKey}`);
+    console.log(`location ${location}`);
+    console.log(`filePath ${filePath}`);
+    console.log(`dirPath ${dirPath}`);
+    console.log(`from ${from}`);
+    console.log(`to ${to}`);
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`);
+  }
+
+  const command = `npm run translate -- -k ${subscriptionKey} -f ${from} -t ${to}`;
+  if (filePath) {
+    command += ` -p ${filePath}`;
+  } else if (dirPath) {
+    command += ` -d ${dirPath}`
+  }
+  if (location) {
+    command += ` -l ${location}`;
+  }
+  if (debug) {
+    console.log('Executing command');
+    console.log(command);
+  }
+  childProcess.execSync(command);
+  // childProcess.execSync(command, { stdio: 'inherit' });
+  console.log(`***Finished translation.`);
+
+  // console.log(`Hello ${provider}`);
   // core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  // const time = (new Date()).toTimeString();
 } catch (error) {
   core.setFailed(error.message);
 }
